92d9abbdfb5b1e380e72ee59b27e1e5028312855
Chiuzu-chilumbu <chiuzubenny@hotmail.com>
2024-06-07 08:56:29 +0800

    added validate and add dragon files

--- ListDragons/listDragons.py
+++ ListDragons/listDragons.py
@@ -1,11 +1,6 @@
 """List Dragons Lambda function handler driver code"""
 import boto3
 import json
-import logging
-
-# Set up logging
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
 
 # Declare variables outside handler to reduce initialization on startup
 s3 = boto3.client('s3', 'ap-northeast-3')
@@ -26,7 +21,6 @@ def listDragons(event, context):
             expression = f"select * from S3Object[*][*] s where s.family_str = '{event['queryStringParameters']['family']}'"
     
     try:
-        logger.info(f"Running query: {expression}")
         result = s3.select_object_content(
             Bucket=bucket_name,
             Key=file_name,
@@ -41,29 +35,13 @@ def listDragons(event, context):
             if 'Records' in event:
                 records += event['Records']['Payload'].decode('utf-8')
     
-        logger.info(f"Query result: {records}")
         return {
             "statusCode": 200,
             "body": json.dumps(records)
         }
     
     except Exception as e:
-        logger.error(f"Error executing query: {str(e)}", exc_info=True)
         return {
             "statusCode": 500,
             "body": json.dumps({"error": str(e)})
         }
-
-# Example event and context for testing
-if __name__ == "__main__":
-    test_event = {
-        "queryStringParameters": {
-            "dragonName": "Atlas",
-            "family": "red"
-        }
-    }
-    context = {}
-
-    # Call the function and print the response
-    response = listDragons(test_event, context)
-    print(response)
